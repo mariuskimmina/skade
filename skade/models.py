d1b3ad704c935ca9dee5ef0fb9e39278015f5a3d
Mindslave <mindslave.blog@protonmail.com>
2021-01-09 12:32:25 +0100

    using uuids

--- skade/models.py
+++ skade/models.py
@@ -1,10 +1,19 @@
 from skade import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Files(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+
+class Reports(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
