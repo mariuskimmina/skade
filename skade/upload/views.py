655d1cfb0924684205b0e839d9e214c5a4b089f0
Mindslave <mindslave.blog@protonmail.com>
2021-01-10 08:15:56 +0100

    added footer

--- skade/upload/views.py
+++ skade/upload/views.py
@@ -3,10 +3,16 @@ from flask_login.utils import login_user, logout_user
 from . import upload
 from skade.models import User
 from flask import render_template, redirect, url_for, request, flash
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required
 from skade.analyze.yararules import yara_scan
 
 
 @upload.route('/', methods=["GET"])
+@login_required
 def main_screen():
     return render_template('main.html')
+
+@upload.route('/upload', methods=["POST"])
+@login_required
+def upload_endpoint():
+    return redirect(url_for('main_screen'))
